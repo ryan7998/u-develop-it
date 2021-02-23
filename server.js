57fd9a80a7026fef719a20ca751098d6c5f3a83c
fazle <ryan7998@gmail.com>
2021-02-23 02:51:18 -0500

    party api finished

--- server.js
+++ server.js
@@ -101,7 +101,78 @@ app.post('/api/candidate', ({body}, res)=>{
     });
 });
 
+// get all Parties:
+app.get('/api/parties', (req, res)=>{
+    const sql = `SELECT *FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows)=>{
+        if(err){
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({
+            message:'succcess',
+            data:rows
+        });
+    });
+});
 
+// get single party:
+app.get('/api/party/:id', (req, res)=>{
+    const sql = `SELECT *FROM parties WHERE id =?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row)=>{
+        if(err){
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message:'success',
+            data:row
+        });
+    });
+});
+
+// delete party:
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({ message: 'successfully deleted', changes: this.changes });
+    });
+});
+
+// update party:
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+    }
+    
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+  
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: this.changes
+      });
+    });
+  });
 
 
 // Default response for any other request (Not Found) Cath all. should always be at the end of other routes as its a catch
